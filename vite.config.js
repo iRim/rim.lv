5806c539e53c4177cb13aba935ba35ddd535aa3e
Ihor Rusenko <rusenko.i@outlook.com>
2022-02-19 18:00:25 +0200

    add vite-base

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,43 @@
-import { defineConfig } from 'vite'
+import path from 'path'
+import {
+  defineConfig
+} from 'vite'
 import vue from '@vitejs/plugin-vue'
+
+const PATHS = {
+  src: path.resolve(__dirname, './src'),
+  output: path.resolve(__dirname, './public_html'),
+  static: path.resolve(__dirname, './src/static'),
+  css: path.resolve(__dirname, './src/scss')
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  root: PATHS.static, // where is index.html
+
+  server: {
+    port: 80
+  },
+
+  resolve: {
+    alias: {
+      '@': PATHS.src
+    },
+  },
+
+  plugins: [
+    vue()
+  ],
+
+  build: {
+    emptyOutDir: true,
+    outDir: PATHS.output,
+
+    rollupOptions: {
+      input: {
+        main: `${PATHS.static}/index.html`,
+      }
+    }
+  },
+
 })
