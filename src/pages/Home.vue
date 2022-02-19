6a7ead7617d831a2f0fe799e01053cadc5043d3a
Ihor Rusenko <rusenko.i@outlook.com>
2022-02-19 21:35:03 +0200

    update

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,8 +1,27 @@
-<template>HOME PAGE</template>
+<template>
+  <div>
+    <b>HOME PAGE</b>
+    <small>test</small>
+  </div>
+</template>
 
 <script>
 export default {};
 </script>
 
-<style>
+<style lang="scss">
+div {
+  > b {
+    font-size: 20px;
+    font-weight: bold;
+    font-style: italic;
+  }
+  > small {
+    font-size: 26px;
+  }
+
+  * {
+    display: block;
+  }
+}
 </style>
--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,6 @@ const PATHS = {
   src: path.resolve(__dirname, './src'),
   output: path.resolve(__dirname, './public_html'),
   static: path.resolve(__dirname, './src/static'),
-  css: path.resolve(__dirname, './src/scss')
 }
 
 // https://vitejs.dev/config/
@@ -32,12 +31,6 @@ export default defineConfig({
   build: {
     emptyOutDir: true,
     outDir: PATHS.output,
-
-    rollupOptions: {
-      input: {
-        main: `${PATHS.static}/index.html`,
-      }
-    }
   },
 
 })
