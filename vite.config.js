bad36ed279e05e2a4d26849d5e7e90ca177905d8
iRim <rusenko.i@outlook.com>
2024-03-24 17:57:51 +0200

    upgrade

--- vite.config.js
+++ vite.config.js
@@ -1,53 +1,82 @@
-import path from 'path'
-import {
-  defineConfig
-} from 'vite'
-import vue from '@vitejs/plugin-vue'
-import copy from 'rollup-plugin-copy'
+import { defineConfig, splitVendorChunkPlugin } from "vite";
+import commonjs from "@rollup/plugin-commonjs";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
-const PATH_INTERNAL = {
-    src: 'src',
-    output: 'public_html',
-    static: 'src/static',
-  },
-  PATH = {
-    src: path.resolve(__dirname, PATH_INTERNAL.src),
-    output: path.resolve(__dirname, PATH_INTERNAL.output),
-    static: path.resolve(__dirname, PATH_INTERNAL.static),
-  }
+const ASSET_URL = process.env.ASSET_URL || "";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  root: PATH.static, // where is index.html
+    base: `${ASSET_URL}/`,
+    root: './src/static',
 
-  server: {
-    port: 80
-  },
-
-  resolve: {
-    alias: {
-      '@': PATH.src
+    server: {
+        host: "0.0.0.0",
+        hmr: {
+            host: "localhost",
+        },
     },
-  },
 
-  plugins: [
-    vue(),
-    copy({
-      targets: [{
-        src: [
-          // `${PATH_INTERNAL.static}/index.html`,
-          `${PATH_INTERNAL.static}/.htaccess`,
-          // `${PATH_INTERNAL.static}/favicon.ico`,
-          // `${PATH_INTERNAL.static}/logo.png`
-        ],
-        dest: PATH_INTERNAL.output
-      }],
-    })
-  ],
+    build: {
+        outDir: path.resolve(__dirname, "public/"),
+        emptyOutDir: true,
+        manifest: true,
+        target: "es2018",
+        rollupOptions: {
+            output: {
+                manualChunks: {
+                    vue: ["vue", "vue-router", "pinia"],
+                },
+                chunkFileNames: "js/[name]-[hash].js",
+                entryFileNames: "js/[name]-[hash].js",
 
-  build: {
-    emptyOutDir: true,
-    outDir: PATH.output,
-  },
+                assetFileNames: ({ name }) => {
+                    if (/\.(gif|jpe?g|png|svg)$/.test(name ?? "")) {
+                        return "img/[name]-[hash][extname]";
+                    }
+                    if (/\.css$/.test(name ?? "")) {
+                        return "css/[name]-[hash][extname]";
+                    }
+                    return "[name]-[hash][extname]";
+                },
+            },
+        },
+        assetsInlineLimit: 0,
+    },
+    plugins: [
+        [commonjs()],
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: true,
+                },
+            },
+        }),
+        viteStaticCopy({
+            targets: [
+                {
+                    src: "@static/*",
+                    dest: "./public",
+                },
+            ],
+        }),
 
-})
+        [splitVendorChunkPlugin()],
+    ],
+    resolve: {
+        alias: {
+            vue: "vue/dist/vue.esm-bundler.js",
+            "@": path.resolve(__dirname, "./src"),
+            "@static": path.resolve(__dirname, "./src/static"),
+            "@components": path.resolve(
+                __dirname,
+                "./src/components"
+            ),
+            "@pages": path.resolve(
+                __dirname,
+                "./src/pages"
+            ),
+        },
+    },
+});
