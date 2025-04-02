e3e69f70fe5a2aa386ae9d526b55009971046d27
iRim <rusenko.i@outlook.com>
2025-04-02 19:04:51 +0300

    update

--- src/vue/views/Framework.vue
+++ src/vue/views/Framework.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="container">
     <div class="row">
-      <div class="column width-10">
+      <div class="column w-10">
         <h1>Heading</h1>
       </div>
       <div class="column">
         <h2>Heading</h2>
       </div>
-      <div class="column width-33 offset-47">
+      <div class="column w-40 o-40">
         <h3>Heading</h3>
       </div>
     </div>
 
-    <table class="bordered striped hoverable">
+    <table class="bordered">
+      <caption>
+        Test table
+      </caption>
       <thead>
         <tr>
           <th>Name</th>
@@ -37,13 +40,32 @@
       </tbody>
       <tfoot>
         <tr>
-          <td colspan="4">
-            <button class="btn btn-primary">Add</button>
-            <button class="btn btn-secondary">Edit</button>
-            <button class="btn btn-danger">Delete</button>
-          </td>
+          <td colspan="3">Pages</td>
+          <td>1.2.3</td>
         </tr>
       </tfoot>
     </table>
+
+    <div class="row p-v">
+      <div class="column p-right w-20 as-column">
+        <div class="btn width-100">default</div>
+        <div class="hr">disabled buttons</div>
+        <div class="btn disabled width-100">disabled class</div>
+        <button class="btn" disabled>disabled attribute</button>
+      </div>
+      <div class="column p-h w-20 as-column">
+        <div class="btn outlined w-100">outlined</div>
+        <div class="hr">outlined disabled buttons</div>
+        <div class="btn outlined disabled width-100">disabled class</div>
+        <button class="btn outlined" disabled>disabled attribute</button>
+      </div>
+
+      <div class="column p-h w-20 as-column">
+        <div class="btn link w-100">link</div>
+        <div class="hr">link disabled buttons</div>
+        <div class="btn link disabled width-100">disabled class</div>
+        <button class="btn link" disabled>disabled attribute</button>
+      </div>
+    </div>
   </div>
 </template>
